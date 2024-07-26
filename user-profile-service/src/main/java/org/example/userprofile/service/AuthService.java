fc222f7a6ef00484e077a112a2fffc1a4152b1e4
SpellZZZ <james321321@wp.pl>
2024-07-26 17:21:24 +0200

    FIX authservice method

--- user-profile-service/src/main/java/org/example/userprofile/service/AuthService.java
+++ user-profile-service/src/main/java/org/example/userprofile/service/AuthService.java
@@ -14,7 +14,7 @@ public class AuthService {
     private final UserProfileService userService;
 
     public void register(UserProfileRequest userProfileRequest) throws Exception {
-        if(userService.isEmailExisting(userProfileRequest.getEmail()) &&
+        if(userService.isEmailExisting(userProfileRequest.getEmail()) ||
                 userService.isUsernameExisting(userProfileRequest.getUsername())) {
             throw new Exception();
         }
