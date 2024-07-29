a6b9e59d859a2a609424f31912d96573f3900c30
SpellZZZ <james321321@wp.pl>
2024-07-30 00:52:12 +0200

    ADD chat service

---
+++ chat-service/src/main/java/org/example/ChatServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserProfileApplication.class, args);
+    }
+
+}
