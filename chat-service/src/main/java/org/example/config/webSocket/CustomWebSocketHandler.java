9f336032481d9e6645df72960cd73f598a6b271f
SpellZZZ <james321321@wp.pl>
2024-08-12 21:11:41 +0200

    FIX refactor config packages

--- chat-service/src/main/java/org/example/config/webSocket/CustomWebSocketHandler.java
+++ chat-service/src/main/java/org/example/config/webSocket/CustomWebSocketHandler.java
@@ -1,16 +1,12 @@
-package org.example.config;
+package org.example.config.webSocket;
 
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.socket.WebSocketHandler;
 import org.springframework.web.reactive.socket.WebSocketSession;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Sinks;
-
-import java.time.Duration;
-import java.util.UUID;
 
 
 @Component
