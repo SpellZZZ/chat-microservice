81b8b991d04cd56f1ab37f2c04db8dd0fb81d27a
SpellZZZ <james321321@wp.pl>
2024-11-05 02:06:10 +0100

    FIX project

--- chat-service/src/main/java/org/example/config/CustomWebSocketHandler.java
+++ chat-service/src/main/java/org/example/config/CustomWebSocketHandler.java
@@ -1,65 +1,43 @@
 package org.example.config;
-
-
 import lombok.RequiredArgsConstructor;
 import org.example.model.ChatMessage;
 import org.example.repository.ChatMessageRepository;
-import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.socket.WebSocketHandler;
 import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.WebSocketSession;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Sinks;
-
 import java.net.URI;
 import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-
-
 @Component
 @RequiredArgsConstructor
 public class CustomWebSocketHandler implements WebSocketHandler {
-
-    //private final ReactiveStringRedisTemplate redisTemplate;
     private final Sinks.Many<String> sinks;
     private final ChatMessageRepository messageRepository;
     private final Map<String, Sinks.Many<String>> userSessions = new ConcurrentHashMap<>();
-
-
     @Override
     public Mono<Void> handle(WebSocketSession session) {
         String userId = getUserIdFromSession(session.getHandshakeInfo().getUri());
         if (userId == null || userId.isEmpty()) {
             return session.close();
         }
-
         Sinks.Many<String> sink = userSessions.computeIfAbsent(userId, k -> Sinks.many().multicast().directBestEffort());
-
-
-        /*redisTemplate.listenToChannel(userId)
-                .map(msg -> msg.getMessage())
-                .doOnNext(sink::tryEmitNext)
-                .subscribe();*/
-
         Mono<Void> input = session.receive()
                 .map(WebSocketMessage::getPayloadAsText)
                 .doOnNext(message -> handleIncomingMessage(message, userId))
                 .then();
-
         Mono<Void> output = session.send(sink.asFlux().map(session::textMessage));
-
         return Mono.zip(input, output).then()
                 .doFinally(signalType -> userSessions.remove(userId));
     }
-
     private void handleIncomingMessage(String message, String senderId) {
         String[] parts = message.split(":", 2);
         if (parts.length == 2) {
             String receiverId = parts[0];
             String content = parts[1];
-
             ChatMessage newMessage = ChatMessage.builder()
                     .message(content)
                     .receiverId(receiverId)
@@ -67,17 +45,18 @@ public class CustomWebSocketHandler implements WebSocketHandler {
                     .timestamp(LocalDateTime.now())
                     .build();
             messageRepository.save(newMessage).subscribe();
-
             System.out.println("Receiver " + receiverId);
             System.out.println("Sender " + senderId);
-
-
-            //redisTemplate.convertAndSend(receiverId, senderId + ": " + content).subscribe();
-
-            // redisTemplate.convertAndSend(senderId, "You: " + content).subscribe(); // old
+            sendMessageToUser(receiverId, /*senderId + ": " +*/ content);
+            //sendMessageToUser(senderId, "You: " + content);
         }
     }
-
+    private void sendMessageToUser(String userId, String message) {
+        Sinks.Many<String> sink = userSessions.get(userId);
+        if (sink != null) {
+            sink.tryEmitNext(message);
+        }
+    }
     private String getUserIdFromSession(URI uri) {
         String query = uri.getQuery();
         if (query != null) {
