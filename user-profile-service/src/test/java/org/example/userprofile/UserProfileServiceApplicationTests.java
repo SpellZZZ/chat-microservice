459c42a17f70d1b6d57bdc23527fe53de9f874ba
SpellZZZ <james321321@wp.pl>
2024-11-06 01:42:01 +0100

    ADD maven wrapper

--- user-profile-service/src/test/java/org/example/userprofile/UserProfileServiceApplicationTests.java
+++ user-profile-service/src/test/java/org/example/userprofile/UserProfileServiceApplicationTests.java
@@ -3,11 +3,7 @@ package org.example.userprofile;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class UserProfileServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
